a2aecda8fb24f26161212f7bb872e2e33f427799
LoL-2003 <148077211+LoL-2003@users.noreply.github.com>
2023-10-21 21:37:29 +0530

    Update login.py

--- login.py
+++ login.py
@@ -204,7 +204,7 @@ from firebase_admin import auth
 import uuid
 import smtplib
 
-
+username = '' 
 def header_footer():
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
     st.markdown("""
