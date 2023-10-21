68b6ea65e621d1fe63b7899e35ab46e606a2a0e2
LoL-2003 <148077211+LoL-2003@users.noreply.github.com>
2023-10-21 14:32:27 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,88 +1,158 @@
-import streamlit as st
-import firebase_admin
-from firebase_admin import firestore
-from firebase_admin import credentials
-from firebase_admin import auth
+# import streamlit as st
+# import firebase_admin
+# from firebase_admin import firestore
+# from firebase_admin import credentials
+# from firebase_admin import auth
 
 
-cred = credentials.Certificate("ec-hack-2023-c35a12353f9e.json")
-#firebase_admin.initialize_app(cred)
-def app():
-# Usernm = []
-    st.title('Welcome to :violet[Pondering] :sunglasses:')
+# cred = credentials.Certificate("ec-hack-2023-c35a12353f9e.json")
+# #firebase_admin.initialize_app(cred)
+# def app():
+# # Usernm = []
+#     st.title('Welcome to :violet[Pondering] :sunglasses:')
 
-    if 'username' not in st.session_state:
-        st.session_state.username = ''
-    if 'useremail' not in st.session_state:
-        st.session_state.useremail = ''
+#     if 'username' not in st.session_state:
+#         st.session_state.username = ''
+#     if 'useremail' not in st.session_state:
+#         st.session_state.useremail = ''
 
 
 
-    def f(): 
-        try:
-            user = auth.get_user_by_email(email)
-            print(user.uid)
-            st.session_state.username = user.uid
-            st.session_state.useremail = user.email
+#     def f(): 
+#         try:
+#             user = auth.get_user_by_email(email)
+#             print(user.uid)
+#             st.session_state.username = user.uid
+#             st.session_state.useremail = user.email
             
-            global Usernm
-            Usernm=(user.uid)
+#             global Usernm
+#             Usernm=(user.uid)
             
-            st.session_state.signedout = True
-            st.session_state.signout = True    
+#             st.session_state.signedout = True
+#             st.session_state.signout = True    
   
             
-        except: 
-            st.warning('Login Failed')
+#         except: 
+#             st.warning('Login Failed')
 
-    def t():
-        st.session_state.signout = False
-        st.session_state.signedout = False   
-        st.session_state.username = ''
+#     def t():
+#         st.session_state.signout = False
+#         st.session_state.signedout = False   
+#         st.session_state.username = ''
 
-
-        
-    
-        
-    if "signedout"  not in st.session_state:
-        st.session_state["signedout"] = False
-    if 'signout' not in st.session_state:
-        st.session_state['signout'] = False    
-        
 
         
     
-    if  not st.session_state["signedout"]: # only show if the state is False, hence the button has never been clicked
-        choice = st.selectbox('Login/Signup',['Login','Sign up'])
-        email = st.text_input('Email Address')
-        password = st.text_input('Password',type='password')
+        
+#     if "signedout"  not in st.session_state:
+#         st.session_state["signedout"] = False
+#     if 'signout' not in st.session_state:
+#         st.session_state['signout'] = False    
         
 
         
-        if choice == 'Sign up':
-            username = st.text_input("Enter  your unique username")
+    
+#     if  not st.session_state["signedout"]: # only show if the state is False, hence the button has never been clicked
+#         choice = st.selectbox('Login/Signup',['Login','Sign up'])
+#         email = st.text_input('Email Address')
+#         password = st.text_input('Password',type='password')
+        
+
+        
+#         if choice == 'Sign up':
+#             username = st.text_input("Enter  your unique username")
             
-            if st.button('Create my account'):
-                user = auth.create_user(email = email, password = password,uid=username)
+#             if st.button('Create my account'):
+#                 user = auth.create_user(email = email, password = password,uid=username)
                 
-                st.success('Account created successfully!')
-                st.markdown('Please Login using your email and password')
-                st.balloons()
-        else:
-            # st.button('Login', on_click=f)          
-            st.button('Login', on_click=f)
+#                 st.success('Account created successfully!')
+#                 st.markdown('Please Login using your email and password')
+#                 st.balloons()
+#         else:
+#             # st.button('Login', on_click=f)          
+#             st.button('Login', on_click=f)
             
             
-    if st.session_state.signout:
-                st.text('Name '+st.session_state.username)
-                st.text('Email id: '+st.session_state.useremail)
-                st.button('Sign out', on_click=t) 
+#     if st.session_state.signout:
+#                 st.text('Name '+st.session_state.username)
+#                 st.text('Email id: '+st.session_state.useremail)
+#                 st.button('Sign out', on_click=t) 
             
                 
     
 
                             
-    def ap():
-        st.write('Posts')
+#     def ap():
+#         st.write('Posts')
 
-app()
+# app()
+
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("ec-hack-2023-c35a12353f9e.json")
+firebase_admin.initialize_app(cred)
+
+def app():
+    st.title('Welcome to :violet[Pondering] :sunglasses:')
+    
+    # Initialize session_state with required keys and values
+    if 'username' not in st.session_state:
+        st.session_state.username = ''
+    if 'useremail' not in st.session_state:
+        st.session_state.useremail = ''
+    if 'signedout' not in st.session_state:
+        st.session_state.signedout = False
+    if 'signout' not in st.session_state:
+        st.session_state.signout = False
+    
+    def f():
+        try:
+            user = auth.get_user_by_email(email)
+            if user.email_verified:
+                st.session_state.username = user.uid
+                st.session_state.useremail = user.email
+                st.session_state.signedout = True
+                st.session_state.signout = True
+            else:
+                st.warning('Email not verified. Please check your email for verification instructions.')
+        except auth.EmailAlreadyExistsError:
+            st.warning('Email address is already in use.')
+        except:
+            st.warning('Login Failed')
+
+    def t():
+        st.session_state.signout = False
+        st.session_state.signedout = False
+        st.session_state.username = ''
+    
+    choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
+    email = st.text_input('Email Address')
+    password = st.text_input('Password', type='password')
+    
+    if choice == 'Sign up':
+        username = st.text_input("Enter your unique username")
+        
+        if st.button('Create my account'):
+            try:
+                user = auth.create_user(email=email, password=password, uid=username)
+                auth.send_email_verification(user.email)  # Send email verification
+                st.success('Account created successfully! Please check your email for verification.')
+                st.markdown('Please Login using your email and password')
+                st.balloons()
+            except auth.EmailAlreadyExistsError:
+                st.warning('Email address is already in use.')
+    else:
+        st.button('Login', on_click=f)
+    
+    if st.session_state.signout:
+        st.text('Name ' + st.session_state.username)
+        st.text('Email id: ' + st.session_state.useremail)
+        st.button('Sign out', on_click=t)
+
+if __name__ == '__main__':
+    app()
+
