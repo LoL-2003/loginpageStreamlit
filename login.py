e20b72de6ad0bbfcbb6e77ccd2387dd55aa1ddc4
LoL-2003 <148077211+LoL-2003@users.noreply.github.com>
2023-10-21 17:06:14 +0530

    Update login.py

--- login.py
+++ login.py
@@ -22,23 +22,41 @@
 #         st.session_state.signedout = False
 #     if 'signout' not in st.session_state:
 #         st.session_state.signout = False
+
+#     def forgot_password():
+#         try:
+#             link = auth.generate_password_reset_link(email)
+#             st.write(link)
+#             st.write('Please check your email for password reset instructions or click on the link above')
+#             st.warning('Password reset link sent to your email')
+#         except auth.UserNotFoundError:
+#             st.warning('User not found. Please check your email address')
+#         except Exception as e:
+#             st.warning('Password reset failed. Error: ' + str(e))
     
 #     def f():
 #         try:
 #             user = auth.get_user_by_email(email)
 #             if user.email_verified:
-#                 st.session_state.username = user.uid
-#                 st.session_state.useremail = user.email
-#                 st.session_state.signedout = True
-#                 st.session_state.signout = True
+#                 if auth.verify_password(user.uid, password):  # Verify the entered password
+#                     st.session_state.username = user.uid
+#                     st.session_state.useremail = user.email
+#                     st.session_state.signedout = True
+#                     st.session_state.signout = True
+#                 else:
+#                     st.warning('Incorrect password')
 #             else:
-#                 auth.send_email_verification(user.email)  # Send email verification for existing user
-#                 st.warning('Email not verified. Please check your email for verification instructions.')
+#                 link = auth.generate_email_verification_link(email)
+#                 st.write(link)
+#                 st.write('Please check your email for verification instructions or click on the link above')
+#                 st.warning('Email not verified.')
+#         except auth.UserNotFoundError:
+#             st.warning('User not found.')
 #         except auth.EmailAlreadyExistsError:
 #             st.warning('The email address is already in use.')
-#         except:
-#             st.warning('Login Failed')
-
+#         except Exception as e:
+#             st.warning('Login Failed. Error: ' + str(e))
+    
 #     def t():
 #         st.session_state.signout = False
 #         st.session_state.signedout = False
@@ -47,6 +65,7 @@
 #     choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
 #     email = st.text_input('Email Address')
 #     password = st.text_input('Password', type='password')
+#     st.markdown('forgot password', on_click=forgot_password)
     
 #     if choice == 'Sign up':
 #         username = st.text_input("Enter your unique username")
@@ -55,28 +74,29 @@
 #             try:
 #                 uid = str(uuid.uuid4())  # Generate a unique UID
 #                 user = auth.create_user(email=email, password=password, uid=uid)
-                
-#                 # Set email_verified to False (user's email is not verified)
-#                 auth.update_user(user.uid, email_verified=False)
-                
-#                 # Send an email verification link to the user's email
-#                 auth.generate_email_verification_link(user.email, action_code_settings=None)
-                
+#                 link = auth.generate_email_verification_link(email)
+#                 st.write(link)
+#                 st.write('Please check your email for verification instructions or click on the link above')
 #                 st.success('Account created successfully! Please check your email for verification.')
 #                 st.markdown('Please Login using your email and password')
 #                 st.balloons()
 #             except auth.EmailAlreadyExistsError:
 #                 st.warning('The email address is already in use.')
+#             except Exception as e:
+#                 st.warning('Account creation failed. Error: ' + str(e))
 #     else:
 #         st.button('Login', on_click=f)
     
 #     if st.session_state.signout:
 #         st.text('Name ' + st.session_state.username)
 #         st.text('Email id: ' + st.session_state.useremail)
+#         st.success('You are logged in!')
 #         st.button('Sign out', on_click=t)
 
 # if __name__ == '__main__':
 #     app()
+
+
 
 
 
@@ -147,7 +167,6 @@ def app():
     choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
     email = st.text_input('Email Address')
     password = st.text_input('Password', type='password')
-    st.markdown('forgot password', on_click=forgot_password)
     
     if choice == 'Sign up':
         username = st.text_input("Enter your unique username")
@@ -167,7 +186,8 @@ def app():
             except Exception as e:
                 st.warning('Account creation failed. Error: ' + str(e))
     else:
-        st.button('Login', on_click=f)
+        if st.button('Login', on_click=f):
+            forgot_password()
     
     if st.session_state.signout:
         st.text('Name ' + st.session_state.username)
@@ -177,3 +197,4 @@ def app():
 
 if __name__ == '__main__':
     app()
+
