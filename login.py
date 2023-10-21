33ee9ca468466edf988c8dd136a5764dcb87cf2e
LoL-2003 <148077211+LoL-2003@users.noreply.github.com>
2023-10-21 14:39:18 +0530

    Update login.py

--- login.py
+++ login.py
@@ -92,23 +92,17 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import auth
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("ec-hack-2023-c35a12353f9e.json")
-# firebase_admin.initialize_app(cred)
+# Check if the Firebase Admin SDK is already initialized
+if not firebase_admin._apps:
+    # Initialize Firebase Admin SDK
+    cred = credentials.Certificate("ec-hack-2023-c35a12353f9e.json")
+    firebase_admin.initialize_app(cred)
 
 def app():
     st.title('Welcome to :violet[Pondering] :sunglasses:')
     
-    # Initialize session_state with required keys and values
-    if 'username' not in st.session_state:
-        st.session_state.username = ''
-    if 'useremail' not in st.session_state:
-        st.session_state.useremail = ''
-    if 'signedout' not in st.session_state:
-        st.session_state.signedout = False
-    if 'signout' not in st.session_state:
-        st.session_state.signout = False
-    
+    # Rest of your code...
+
     def f():
         try:
             user = auth.get_user_by_email(email)
@@ -119,8 +113,6 @@ def app():
                 st.session_state.signout = True
             else:
                 st.warning('Email not verified. Please check your email for verification instructions.')
-        except auth.EmailAlreadyExistsError:
-            st.warning('Email address is already in use.')
         except:
             st.warning('Login Failed')
 
@@ -128,26 +120,29 @@ def app():
         st.session_state.signout = False
         st.session_state.signedout = False
         st.session_state.username = ''
-    
+
     choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
     email = st.text_input('Email Address')
     password = st.text_input('Password', type='password')
-    
+
     if choice == 'Sign up':
         username = st.text_input("Enter your unique username")
-        
+
         if st.button('Create my account'):
             try:
                 user = auth.create_user(email=email, password=password, uid=username)
-                auth.send_email_verification(user.email)  # Send email verification
+
+                # Set email_verified to False (user's email is not verified)
+                auth.update_user(user.uid, email_verified=False)
+
                 st.success('Account created successfully! Please check your email for verification.')
                 st.markdown('Please Login using your email and password')
                 st.balloons()
             except auth.EmailAlreadyExistsError:
-                st.warning('Email address is already in use.')
+                st.warning('Email address is already in use')
     else:
         st.button('Login', on_click=f)
-    
+
     if st.session_state.signout:
         st.text('Name ' + st.session_state.username)
         st.text('Email id: ' + st.session_state.useremail)
@@ -155,4 +150,3 @@ def app():
 
 if __name__ == '__main__':
     app()
-
